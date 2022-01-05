6e80da99603bd0b4cde554af9304f1b719fa00d0
webpwnized <webpwnized@gmail.com>
2022-01-04 19:59:18 -0500

    1.0.12 Added module vars

---
+++ modules/kubernetes/variables.tf
@@ -0,0 +1,30 @@
+variable "project" {
+	type	= string
+	default	= ""
+}
+
+variable "location" {
+	type	= string
+	default	= ""
+}
+
+variable "network" {
+	type	= string
+	default	= ""
+}
+
+variable "subnetwork" {
+	type	= string
+	default	= ""
+}
+
+variable "vm-machine-type" {
+	type	= string
+	default	= ""
+}
+
+variable "vm-boot-disk-type" {
+	type	= string
+	default	= ""
+}
+
