5e315a8aa91f31d23c99a5b49943e28e673e1750
webpwnized <webpwnized@gmail.com>
2022-03-16 15:50:11 -0400

    1.1.31 Updated cloud armor policy to include OWASP mod_security rules

--- gcp/cloud-armor-policy.tf
+++ gcp/cloud-armor-policy.tf
@@ -1,5 +1,7 @@
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_security_policy
+# https://cloud.google.com/armor/docs/configure-security-policies
+# https://cloud.google.com/armor/docs/rules-language-reference
 
 locals {
 	security-policy-project	= "${google_compute_network.gcp_vpc_network.project}"
@@ -11,4 +13,40 @@ resource "google_compute_security_policy" "security-policy" {
 	project		= "${local.security-policy-project}"
 	name		= "${local.security-policy-name}"
 	description	= "${local.security-policy-description}"
+
+	rule {
+		description	= "Block access if OWASP rule triggered"
+		action		= "deny(403)"
+		priority	= "1000"
+		match {
+			expr {
+				expression = "evaluatePreconfiguredExpr('sqli-stable') or evaluatePreconfiguredExpr('xss-stable') or evaluatePreconfiguredExpr('lfi-stable') or evaluatePreconfiguredExpr('rfi-stable') or evaluatePreconfiguredExpr('rce-stable')"
+			}
+		}
+	}
+
+	rule {
+		description	= "Allow access to Load Balancer from office"
+		action		= "allow"
+		priority	= "1001"
+		match {
+			versioned_expr	= "SRC_IPS_V1"
+			config {
+				src_ip_ranges	= "${var.admin-office-ip-address-range}"
+			}
+		}
+	}
+
+	rule {
+		description	= "Default rule. Deny all access to Load Balancer"
+		action		= "deny(403)"
+		priority	= "2147483647"
+		match {
+			versioned_expr	= "SRC_IPS_V1"
+			config {
+				src_ip_ranges	= ["*"]
+			}
+		}
+	}
+
 }
