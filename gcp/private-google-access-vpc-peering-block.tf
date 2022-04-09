499e2b9acd55dc0934ac9d7c9e98c258318b7d5b
webpwnized <webpwnized@gmail.com>
2022-04-09 18:22:01 -0400

    1.0.37 Adding MySQL to GCP

---
+++ gcp/private-google-access-vpc-peering-block.tf
@@ -0,0 +1,24 @@
+
+locals {
+	private-vpc-network-self-link	= "${google_compute_network.gcp_vpc_network.self_link}"
+}
+
+resource "google_compute_global_address" "private_ip_block" {
+	provider	= google-beta
+	name		= "private-ip-block"
+	description	= "IP block to peer with Google network for private service connection"
+	network		= "${local.private-vpc-network-self-link}"
+	purpose		= "VPC_PEERING"
+	address_type	= "INTERNAL"
+	ip_version	= "IPV4"
+	labels		= "${var.default-labels}"
+	prefix_length	= 24
+	address		= "10.0.4.0"
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+	provider		= google-beta
+	network			= "${local.private-vpc-network-self-link}"
+	service			= "servicenetworking.googleapis.com"
+	reserved_peering_ranges	= ["${google_compute_global_address.private_ip_block.name}"]
+}
