51208bcf9d53011345db97736ec44b46d2114813
webpwnized <webpwnized@gmail.com>
2022-04-09 20:39:59 -0400

    1.0.37 Adding MySQL to GCP

--- gcp/my-sql.tf
+++ gcp/my-sql.tf
@@ -26,26 +26,6 @@ locals {
 						}"
 }
 
-resource "google_compute_global_address" "private_ip_block" {
-	provider	= google-beta
-	name		= "private-ip-block"
-	description	= "IP block to peer with Google network for private service connection"
-	network		= "${local.mysql-network-self-link}"
-	purpose		= "VPC_PEERING"
-	address_type	= "INTERNAL"
-	ip_version	= "IPV4"
-	labels		= "${var.default-labels}"
-	prefix_length	= 24
-	address		= "10.0.4.0"
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-	provider		= google-beta
-	network			= "${local.mysql-network-self-link}"
-	service			= "servicenetworking.googleapis.com"
-	reserved_peering_ranges	= ["${google_compute_global_address.private_ip_block.name}"]
-}
-
 resource "google_sql_user" "mysql-account" {
 	instance	= "${google_sql_database_instance.mysql.name}"
 	name		= "mutillidae"
