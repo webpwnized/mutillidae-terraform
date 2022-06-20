c21b94fe06aeea7eeec2b6dcf76023de5011228a
webpwnized <webpwnized@gmail.com>
2022-06-20 19:41:23 -0400

    1.0.48 Updated lab destroy script

--- lab-environment/gcp/destroy-gcp-lab-environment.sh
+++ lab-environment/gcp/destroy-gcp-lab-environment.sh
@@ -8,6 +8,10 @@ for i in $(cat terraform-files.txt); do rm $i; done
 # Delete the cloud-init files
 rm -rf cloud-init;
 
-echo "Remaining Files";
+echo "";
+echo "NOTE: There should be 6 files remaining after destruction. If there are more, you probably created lab files that are left over. Its safe to delete files that you created."
+echo "";
+echo "$(ls -1 | wc -l) files remaining";
+echo "---------------------";
 echo "";
 ls -1;
