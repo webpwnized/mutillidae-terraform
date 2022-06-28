84087376121d277bf2624cac99164ef07b8ee551
webpwnized <webpwnized@gmail.com>
2022-06-28 14:35:00 -0400

    1.0.50 Added firewall rule allowing IAP to Utility Server

--- reference-architecture/gcp/utility-server-ingress-firewall-rules.tf
+++ reference-architecture/gcp/utility-server-ingress-firewall-rules.tf
@@ -1,13 +1,30 @@
-locals {
-	utility-server-firewall-project		= "${google_compute_network.gcp_vpc_network.project}"
-	utility-server-firewall-region		= "${var.region}"
-	utility-server-firewall-network-name	= "${google_compute_network.gcp_vpc_network.name}"
+
+resource "google_compute_firewall" "allow-ingress-ssh-iap-to-utility-server" {
+	project		= "${google_compute_network.gcp_vpc_network.project}"
+	name		= "allow-ingress-ssh-iap-to-utility-server"
+	network 	= "${google_compute_network.gcp_vpc_network.name}"
+	description	= "Allow SSH to utility host when using Identity Aware Proxy to tunnel traffic from the Internet"
+	direction	= "INGRESS"
+	disabled	= "false"
+	priority	= 1000
+
+	allow {
+		protocol	= "tcp"
+		ports		= ["${var.ssh-port}"]
+	}
+
+	source_ranges	= var.gcp-iap-ip-address-range
+	target_tags	= ["utility-server"]
+
+	log_config {
+		metadata	= "INCLUDE_ALL_METADATA"
+	}
 }
 
 resource "google_compute_firewall" "allow-ssh-to-utility-server-from-bastion-host" {
-	project		= "${local.utility-server-firewall-project}"
+	project		= "${google_compute_network.gcp_vpc_network.project}"
 	name		= "allow-ssh-to-utility-server-from-bastion-host"
-	network 	= "${local.utility-server-firewall-network-name}"
+	network 	= "${google_compute_network.gcp_vpc_network.name}"
 	description	= "Allow SSH to utility host from the bastion host in the ${local.utility-server-firewall-network-name} network"
 	direction	= "INGRESS"
 	disabled	= "false"
