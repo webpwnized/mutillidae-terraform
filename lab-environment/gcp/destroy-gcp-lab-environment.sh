0cae7a279695d52e4291e68fbf9a2bbfef433333
webpwnized <webpwnized@gmail.com>
2022-07-26 20:17:49 -0400

    1.0.54 Updated destroy script to remove project metadata

--- lab-environment/gcp/destroy-gcp-lab-environment.sh
+++ lab-environment/gcp/destroy-gcp-lab-environment.sh
@@ -8,8 +8,24 @@ for i in $(cat terraform-files.txt); do rm $i; done
 # Delete the cloud-init files
 rm -rf cloud-init;
 
+KEYS=$(gcloud compute project-info describe --format="json" | jq -rc '.commonInstanceMetadata.items[].key');
+
+if [[ $KEYS != "" ]]; then
+
+	echo "";
+	echo "Removing project metadata";
+	echo "";
+
+	for KEY in $KEYS; do
+		echo "Removing key $KEY"; 
+		gcloud compute project-info remove-metadata --keys=$KEY
+		echo "Removed key $KEY";
+	done;
+	echo "";
+fi;
+
 echo "";
-echo "NOTE: There should be 6 files remaining after destruction. If there are more, you probably created lab files that are left over. Its safe to delete files that you created."
+echo "NOTE: There should be 7 files remaining after destruction. If there are more, you probably created lab files that are left over. Its safe to delete files that you created."
 echo "";
 echo "$(ls -1 | wc -l) files remaining";
 echo "---------------------";
