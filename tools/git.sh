a471b6caee7ca40a3715b6e4f54db1a866cce6d3
webpwnized <webpwnized@gmail.com>
2024-02-18 13:40:42 -0500

    1.0.120 Update project services file

--- tools/git.sh
+++ tools/git.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+# Check if the number of command-line arguments is not equal to 2
 if (( $# != 2 ))
 then
+    # Print usage message to standard error
     printf "%b" "Usage: git.sh <version> <annotation>\n" >&2
+    # Exit with error status
     exit 1
 fi
 
+# Assign command-line arguments to variables
 VERSION=$1
 ANNOTATION=$2
 
-echo "Updating version file with version $VERSION"
-echo $VERSION > version
-
+# Print message indicating the tag creation process
 echo "Creating tag $VERSION with annotation \"$ANNOTATION\""
+# Create an annotated tag with the specified version and annotation
 git tag -a $VERSION -m "$ANNOTATION"
 
+# Print message indicating the local commit
 echo "Commiting version $VERSION to local branch"
+# Commit all changes with the version and annotation as the commit message
 git commit -a -m "$VERSION $ANNOTATION"
 
+# Print message indicating the tag push
 echo "Pushing tag $VERSION"
+# Push the newly created tag to the remote repository
 git push --tag
 
+# Print message indicating the version push
 echo "Pushing version $VERSION to upstream"
+# Push the changes to the remote repository
 git push
-
